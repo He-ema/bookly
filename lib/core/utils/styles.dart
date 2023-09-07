7254084db432a3d5cd9d717a4eea74222c1ac032
ibrahim Osama <zx.hema72@gmail.com>
2023-09-07 15:48:34 +0300

    best seller list view item part one

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 abstract class Styles {
-  static const titleMedium =
-      TextStyle(fontSize: 21, fontWeight: FontWeight.w600);
+  static const titleMedium = TextStyle(
+    fontSize: 21,
+    fontWeight: FontWeight.w600,
+  );
 }
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/core/utils/assets.dart';
 import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
@@ -24,7 +25,40 @@ class HomeViewBody extends StatelessWidget {
             style: Styles.titleMedium,
           ),
         ),
+        BestSelllerLestViewItem(),
       ],
+    );
+  }
+}
+
+class BestSelllerLestViewItem extends StatelessWidget {
+  const BestSelllerLestViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 150,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.7 / 4,
+            child: Container(
+              // height: MediaQuery.of(context).size.height * 0.3,
+              // width: MediaQuery.of(context).size.width * 0.4,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                image: const DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage(AssetsData.testImage),
+                ),
+              ),
+            ),
+          ),
+          Column(
+            children: [],
+          )
+        ],
+      ),
     );
   }
 }
